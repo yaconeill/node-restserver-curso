e39972d99adab9f0694e7c0dca18fd225c182ac4
Yaconeill <yaconeill.amq@gmail.com>
2018-07-30 12:47:04 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:-Las3r2018-@ds259111.mlab.com:59111/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
